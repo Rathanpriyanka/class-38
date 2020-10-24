62f2ff32a3183204b70f91f12a448f42b32490c8
Rathanpriyanka <70433334+Rathanpriyanka@users.noreply.github.com>
2020-10-24 16:12:57 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,72 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(100,200,50,50);
+    car2 = createSprite(200,200,50,50);
+    car3 = createSprite(300,200,50,50);
+    car4 = createSprite(400,200,50,50);
+   cars = [car1,car2,car3,car4];
+
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var index=0;
+      var x=0;
+      var y=displayHeight;
+      
+      for(var plr in allPlayers){
+        index=index+1;
+        x=x+100;
+        y=displayHeight-allPlayers[plr].distance;
+        if (index === player.index){
+          cars[index-1].shapeColor="red";
+          console.log(0);
+          console.log(y);
+          camera.position.x=displayWidth/2;
+          camera.position.y=cars[index-1].y;
+          drawSprites(); 
+        }
+         
+       }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      //console.log("entered");
+      player.distance +=50
+      player.update();
+    }
+  }
+}
